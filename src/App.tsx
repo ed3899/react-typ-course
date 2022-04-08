1fe3f6b9701e217a888e22ac3e7f29cafbd0f6c4
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-08 11:31:45 -0500

    before merge, removed unneccesary comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import SearchBox from "./components/search-box/search-box.component";
 
 import "./App.css";
 
-interface App2Props {}
-interface App2State {
-  monsters: {name: string; id: string}[];
-  searchField: string;
-}
-
 const App = function () {
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState<{name: string; id: string}[]>([]);
@@ -47,52 +41,5 @@ const App = function () {
     </div>
   );
 };
-
-// class App2 extends Component<App2Props, App2State> {
-//   constructor(props: App2Props) {
-//     super(props);
-
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-
-//     this.onSearchChange = this.onSearchChange.bind(this);
-//   }
-
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then(res => res.json())
-//       .then(users => this.setState(() => ({monsters: users})));
-//   }
-
-//   onSearchChange(event: React.ChangeEvent<HTMLInputElement>) {
-//     const searchField = event.target.value.toLowerCase();
-
-//     this.setState({searchField});
-//   }
-
-//   render(): React.ReactNode {
-//     // Computed properties
-//     const {monsters, searchField} = this.state;
-//     const {onSearchChange} = this;
-
-//     const filteredMonsters = monsters.filter(monster => {
-//       return monster.name.toLowerCase().includes(searchField);
-//     });
-
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={onSearchChange}
-//           placeholder="search monsters"
-//           className="search-box"
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -21,19 +21,4 @@ const CardList = function (props: CardListProps) {
   );
 };
 
-// class CardList extends Component<CardListProps> {
-//   render() {
-//     const {monsters} = this.props;
-
-//     return (
-//       <div className="card-list">
-//         {monsters.map(monster => {
-//           const {id} = monster;
-//           return <Card monster={monster} key={id} />;
-//         })}
-//       </div>
-//     );
-//   }
-// }
-
 export default CardList;
